edac0cbcdc9ea30858ded9d9c8fcadd627e95130
shield_omega <yanghanlan666@gmail.com>
2023-08-16 03:05:50 -0400

    by @nrkumar93 . added ceiling filter to remove disturbance. now gives a clean signal

--- scripts/lidar_ball_detector.py
+++ scripts/lidar_ball_detector.py
@@ -13,20 +13,32 @@ previous_scan_2 = LaserScan()
 
 def LidarCallback1(msg):
     global previous_scan_1
-    if previous_scan_1.ranges.size == 0:
+    if len(previous_scan_1.ranges) == 0:
         previous_scan_1 = msg
         return
 
-    scan1_diff = np.linalg.norm(msg.ranges - previous_scan_1.ranges)
+    # ceiling filter
+    ranges = np.array(previous_scan_1.ranges)
+    filt_ranges = ranges[ranges < 2]
+
+    # scan1_diff = np.linalg.norm(np.array(msg.ranges) - np.array(previous_scan_1.ranges))
+    scan1_diff = np.linalg.norm(filt_ranges)
+    previous_scan_1 = msg
     pub1.publish(scan1_diff)
 
 def LidarCallback2(msg):
     global previous_scan_2
-    if previous_scan_2.ranges.size == 0:
+    if len(previous_scan_2.ranges) == 0:
         previous_scan_2 = msg
         return
 
-    scan2_diff = np.linalg.norm(msg.ranges - previous_scan_2.ranges)
+    # ceiling filter
+    ranges = np.array(previous_scan_2.ranges)
+    filt_ranges = ranges[ranges < 2]
+
+    # scan2_diff = np.linalg.norm(np.array(msg.ranges) - np.array(previous_scan_2.ranges))
+    scan2_diff = np.linalg.norm(filt_ranges)
+    previous_scan_2 = msg
     pub2.publish(scan2_diff)
 
 def LidarListener():
