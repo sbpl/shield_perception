68c67c6269a848a2b60754c7512994cca88f446e
QintongXie <122809654+QintongXie@users.noreply.github.com>
2023-09-07 14:23:25 -0400

    Update stereo depth and trajectory estimation

--- improvement/perception_stereo_depth.py
+++ improvement/perception_stereo_depth.py
@@ -1,99 +1,170 @@
-#!/usr/bin/env python
-
 import rospy
-from sensor_msgs.msg import Image, PointCloud2
-from cv_bridge import CvBridge
 import cv2
-import message_filters
 import numpy as np
-import sensor_msgs.point_cloud2 as pc2
+import sys
+import pyzed.sl as sl
+import cv2 as cv
+from matplotlib import pyplot as plt
+from scipy.optimize import least_squares
 
 
-min_radius = 1  # Minimum radius of the ball
-max_radius = 100  # Maximum radius of the ball
-Baseline = 0.12 # Unit is meter
-focal_length = 500
+# ZED Camera constants
+# For HD720
+Baseline = 0.12  # Use meter units
+fx = 531.2548217773438
+fy = 531.2548217773438
+cx = 666.2540283203125
+cy = 349.59100341796875
 
-
-def callback(rgb_msg, disparity_msg, pc_msg, confidence_msg):
-    bridge = CvBridge()
-    rgb_image = bridge.imgmsg_to_cv2(rgb_msg, desired_encoding='bgr8')
-    disparity_image = bridge.imgmsg_to_cv2(disparity_msg, desired_encoding='passthrough')
-    point_cloud = bridge.imgmsg_to_cv2(pc_msg, desired_encoding='passthrough')
-    confidence_image = bridge.imgmsg_to_cv2(confidence_msg, desired_encoding='passthrough')
-
-    # Convert ZED Mat objects to numpy arrays
-    confidence_map_np = confidence_image
-    disparity_map_np = disparity_image
-
-    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
-
-    # Define the adjusted range for bright orange color in HSV
-    lower_bound = np.array([8, 150, 150])
-    upper_bound = np.array([20, 255, 255])
-
-    # Create a binary mask for orange color in HSV
-    mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
-
-    # Find contours in the mask
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    filtered_points = []
-
-    for contour in contours:
-        (x, y), radius = cv2.minEnclosingCircle(contour)
-        center = (int(x), int(y))
-        radius = int(radius)
-
-        if radius < min_radius or radius > max_radius:
-            continue
-        
-        # Draw the circle
-        cv2.circle(rgb_image, center, radius, (0, 255, 0), 2)
-        # x, y, w, h = cv2.boundingRect(contour)
-        # cv2.rectangle(rgb_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-        # Calculate centroid of the circle
-        centroid_x = int(x)
-        centroid_y = int(y)
-
-        # Get confidence and depth values
-        confidence = confidence_map_np[centroid_x, centroid_y]
-        
-        disparity = disparity_map_np[centroid_x, centroid_y]
-
-        depth = Baseline * focal_length / disparity
-
-        if depth < 5 and confidence < 40:
-
-            # Get the corresponding point cloud value at the centroid pixel
-            point_cloud_value = point_cloud[centroid_x, centroid_y]
-
-            # Append the filtered point with X, Y, Z coordinates
-            filtered_points.append([point_cloud_value[0], point_cloud_value[1], point_cloud_value[2]])
-
-    # Publish the filtered point cloud
-    if filtered_points:
-        filtered_pc = pc2.create_cloud_xyz32(pc_msg.header, filtered_points)
-        pub.publish(filtered_pc)
-
-    cv2.imshow('RGB Image', rgb_image)
-    cv2.waitKey(1)
+# Simulated measurement data (time, x, y, z)
+measurements = []
 
 def main():
-    rospy.init_node('zed_subscriber', anonymous=True)
-    pub = rospy.Publisher('/zed/filtered_point_cloud', PointCloud2, queue_size=10)
+    # Init a rospy node
+    rospy.init_node('zed_perception')
 
-    # Subscribe to the RGB, Depth, Point Cloud, and Confidence topics using message_filters
-    rgb_sub = message_filters.Subscriber('/zed/rgb/image_rect_color', Image)
-    disparity_sub = message_filters.Subscriber('/zed/disparity/disparity_image', Image)
-    point_cloud_sub = message_filters.Subscriber('/zed/point_cloud/cloud_registered', PointCloud2)
-    confidence_sub = message_filters.Subscriber('/zed/confidence/confidence_map', Image)
+    # Create a ZED camera object
+    zed = sl.Camera()
 
-    ts = message_filters.TimeSynchronizer([rgb_sub, disparity_sub, point_cloud_sub, confidence_sub], 10)
-    ts.registerCallback(callback)
+    # Set configuration parameters
+    input_type = sl.InputType()
+    if len(sys.argv) >= 2:
+        input_type.set_from_svo_file(sys.argv[1])
+    init = sl.InitParameters(input_t=input_type)
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.depth_mode = sl.DEPTH_MODE.ULTRA
+    init.coordinate_units = sl.UNIT.METER  # Use meter units
 
-    rospy.spin()
+    # Open the camera
+    err = zed.open(init)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print(repr(err))
+        zed.close()
+        exit(1)
 
-if __name__ == '__main__':
+    # Create and set RuntimeParameters after opening the camera
+    runtime_parameters = sl.RuntimeParameters()
+    runtime_parameters.confidence_threshold = 50
+    runtime_parameters.texture_confidence_threshold = 100
+
+    # Create some variables to store centroid and depth information
+    left_centroid = None
+    right_centroid = None
+
+    while True:
+        # A new image is available if grab() returns SUCCESS
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            # Retrieve left and right images
+            image_sl_left = sl.Mat()
+            zed.retrieve_image(image_sl_left, sl.VIEW.LEFT)
+            image_cv_left = image_sl_left.get_data()
+
+            image_sl_right = sl.Mat()
+            zed.retrieve_image(image_sl_right, sl.VIEW.RIGHT)
+            image_cv_right = image_sl_right.get_data()
+
+            hsv_left_image = cv2.cvtColor(image_cv_left, cv2.COLOR_BGR2HSV)
+            hsv_right_image = cv2.cvtColor(image_cv_right, cv2.COLOR_BGR2HSV)
+
+            # Define the range for orange color in HSV
+            lower_bound = np.array([10, 150, 150])
+            upper_bound = np.array([20, 255, 255])
+
+            # Create a binary mask for orange color in HSV
+            mask_left = cv2.inRange(hsv_left_image, lower_bound, upper_bound)
+            mask_right = cv2.inRange(hsv_right_image, lower_bound, upper_bound)
+
+            # Find left contours in the mask
+            contours_left, _ = cv2.findContours(mask_left, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours_left:
+                # Fit a circle to the contour
+                (x, y), radius = cv2.minEnclosingCircle(contour)
+                center = (int(x), int(y))
+                radius = int(radius)
+
+                # Apply radius threshold
+                if 1 <= radius <= 30:
+                    left_centroid = center
+                    cv2.circle(image_cv_left, center, radius, (0, 255, 0), 2)
+                    t = rospy.Time.now()
+                    measurements.append((t, x, y, 0.0))
+
+            # Find right contours in the mask
+            contours_right, _ = cv2.findContours(mask_right, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours_right:
+                # Fit a circle to the contour
+                (x, y), radius = cv2.minEnclosingCircle(contour)
+                center = (int(x), int(y))
+                radius = int(radius)
+
+                # Apply radius threshold
+                if 1 <= radius <= 30:
+                    right_centroid = center
+                    cv2.circle(image_cv_right, center, radius, (0, 255, 0), 2)
+
+            # Check if centroids were detected in both views
+            if left_centroid is not None and right_centroid is not None:
+                left_x, left_y = left_centroid
+                right_x, right_y = right_centroid
+
+                # Calculate disparity for the detected points
+                disparity = left_x - right_x
+
+                # Calculate 3D global coordinates
+                if disparity != 0:
+                    Z = Baseline * fx / disparity
+                    if 0.1 < Z < 5:
+                        X = Z * (left_x - cx) / fx 
+                        Y = Z * (left_y - cy) / fy 
+                        # Append the filtered point with X, Y, Z coordinates
+                        measurements.append((X, Y, Z))
+
+                    # Perform trajectory estimation here using the measurements
+                    estimated_params = estimate_trajectory(measurements)
+
+
+            # Display the images
+            cv2.imshow("Left Image", image_cv_left)
+            cv2.imshow("Right Image", image_cv_right)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+    cv2.destroyAllWindows()
+    zed.close()
+
+
+
+# Define the mathematical model for the trajectory
+def trajectory_model(params, t):
+    x0, y0, z0, vx0, vy0, vz0 = params
+    t_squared = t**2
+    x = x0 + vx0 * t
+    y = y0 + vy0 * t
+    z = z0 + vz0 * t - 0.5 * 9.81 * t_squared 
+    return np.array([x, y, z])
+
+# Define the error function to minimize (sum of squared errors)
+def error_function(params):
+    errors = []
+    for t, observed_x, observed_y, observed_z in measurements:
+        predicted_position = trajectory_model(params, t)
+        errors.extend([observed_x - predicted_position[0], observed_y - predicted_position[1], observed_z - predicted_position[2]])
+    return errors
+
+# Function to estimate the trajectory parameters using least squares
+def estimate_trajectory(measurements):
+    # Initial guess for the model parameters (adjust as needed)
+    initial_params = (0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+
+    # Use least squares optimization to estimate the model parameters
+    result = least_squares(error_function, initial_params)
+
+    # Extract the optimized parameters representing initial position and velocity
+    x0, y0, z0, vx0, vy0, vz0 = result.x
+    return (x0, y0, z0, vx0, vy0, vz0)
+
+if __name__ == "__main__":
     main()
