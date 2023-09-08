d3bd5d3ce63fddafaec5234234481155918bfbbc
QintongXie <122809654+QintongXie@users.noreply.github.com>
2023-09-07 23:00:54 -0400

    Update trajectory estimation

--- improvement/perception_stereo_depth.py
+++ improvement/perception_stereo_depth.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import cv2
 import numpy as np
@@ -6,6 +8,7 @@ import pyzed.sl as sl
 import cv2 as cv
 from matplotlib import pyplot as plt
 from scipy.optimize import least_squares
+from std_msgs.msg import Float64MultiArray
 
 
 # ZED Camera constants
@@ -16,7 +19,7 @@ fy = 531.2548217773438
 cx = 666.2540283203125
 cy = 349.59100341796875
 
-# Simulated measurement data (time, x, y, z)
+Num_Frame = 3
 measurements = []
 
 def main():
@@ -44,14 +47,24 @@ def main():
 
     # Create and set RuntimeParameters after opening the camera
     runtime_parameters = sl.RuntimeParameters()
-    runtime_parameters.confidence_threshold = 50
+    runtime_parameters.confidence_threshold = 100
     runtime_parameters.texture_confidence_threshold = 100
 
-    # Create some variables to store centroid and depth information
-    left_centroid = None
-    right_centroid = None
+    # SVO file frame rate
+    svo_parameters = zed.get_svo_runtime_parameters()
+    frame_rate = svo_parameters.get_svo_real_time_mode()
+    print(f"Frame Rate: {frame_rate} FPS")
 
-    while True:
+    # # Create ROS publisher for (x0, y0, z0, vx0, vy0, vz0)
+    # rospy.init_node('zed_trajectory_publisher')
+    # pub = rospy.Publisher('/zed/trajectory', Float64MultiArray, queue_size=10)
+
+    # Capture images
+    i = 0
+    count = 0
+    t1 = None  # Initialize t1
+    
+    while i < 1000:
         # A new image is available if grab() returns SUCCESS
         if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
             # Retrieve left and right images
@@ -67,7 +80,7 @@ def main():
             hsv_right_image = cv2.cvtColor(image_cv_right, cv2.COLOR_BGR2HSV)
 
             # Define the range for orange color in HSV
-            lower_bound = np.array([10, 150, 150])
+            lower_bound = np.array([8, 150, 150])
             upper_bound = np.array([20, 255, 255])
 
             # Create a binary mask for orange color in HSV
@@ -76,6 +89,10 @@ def main():
 
             # Find left contours in the mask
             contours_left, _ = cv2.findContours(mask_left, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Create some variables to store the centroids
+            left_centroid = None
+            right_centroid = None
 
             for contour in contours_left:
                 # Fit a circle to the contour
@@ -87,8 +104,6 @@ def main():
                 if 1 <= radius <= 30:
                     left_centroid = center
                     cv2.circle(image_cv_left, center, radius, (0, 255, 0), 2)
-                    t = rospy.Time.now()
-                    measurements.append((t, x, y, 0.0))
 
             # Find right contours in the mask
             contours_right, _ = cv2.findContours(mask_right, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -116,13 +131,30 @@ def main():
                 if disparity != 0:
                     Z = Baseline * fx / disparity
                     if 0.1 < Z < 5:
+                        count = count + 1
                         X = Z * (left_x - cx) / fx 
                         Y = Z * (left_y - cy) / fy 
-                        # Append the filtered point with X, Y, Z coordinates
-                        measurements.append((X, Y, Z))
 
-                    # Perform trajectory estimation here using the measurements
-                    estimated_params = estimate_trajectory(measurements)
+                        if t1 is None:
+                            t1 = rospy.Time.now()
+                            t = 0.0
+                        else:
+                            t = (rospy.Time.now() - t1).to_sec()
+                        
+                        measurements.append((t, X, Y, Z))
+
+                    if count >= Num_Frame:
+                        # Perform trajectory estimation here using the measurements
+                        estimated_params = estimate_trajectory(measurements)
+                        print(f"Estimated Parameters: {estimated_params}")
+                        
+                        # # Publish (x0, y0, z0, vx0, vy0, vz0) as a ROS message
+                        # traj_msg = Float64MultiArray()
+                        # traj_msg.data = estimated_params
+                        # pub.publish(traj_msg)
+
+                        measurements.clear()
+                        t1 = None
 
 
             # Display the images
